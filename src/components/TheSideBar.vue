d00d88957c7903c86a75a9d7b39b106aceace5fb
Mykola <46628437+mudmykola@users.noreply.github.com>
2022-07-07 01:26:19 +0300

    done=>sidebar-style

---
+++ src/components/TheSideBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+data: () => ({
+    selectItem: 3,
+    drawer: null,
+    items: [
+        {
+            icon: 'fas fa-tv'
+        },
+         {
+            icon: 'far fa-building'
+        },
+         {
+            icon: 'far fa-compass'
+        },
+        {
+            icon: 'far fa-folder-open'
+        },
+        {
+            icon: 'far fa-clock'
+        },
+        {
+            icon: 'far fa-bell'
+        },
+        {
+            icon: 'far fa-adress-book'
+        },
+    ],
+})
+}
+</script>
+
+<style>
+.border{
+    margin: 0 15px;
+    background: #a9ddd8;
+    border-radius: 50%;
+    text-decoration: none;
+}
+</style>
---
+++ src/views/TheHome.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app :style="{background: $vuetify.theme.themes.dark.background}">
+<the-side-bar/>
+  </v-app>
+</template>
+
+<script>
+
+import TheSideBar from '@/components/TheSideBar.vue'
+
+  export default {
+    name: 'TheHome',
+
+    components: {
+    
+    TheSideBar
+},
+  }
+</script>
