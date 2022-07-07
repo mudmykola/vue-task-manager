e894aa75184ca73a43a3320816afae7b5e66d6e2
Mykola <46628437+mudmykola@users.noreply.github.com>
2022-07-08 00:17:46 +0300

    add=>rightsidebar

---
+++ src/components/TheRightSideBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+data: () =>({
+  files: [
+         {
+        avatar: 'image-1.png',
+        time: '2 Mins Ago',
+        subtitle: 'QA Automatic',
+        title: '<strong>Miki Roi  </strong>',
+      },
+           {
+        avatar: 'image-2.png',
+        time: '10 Mins Ago',
+        subtitle: 'Backand Developer',
+        title: '<strong>Simo Muore </strong>',
+      },
+         {
+        avatar: 'image-3.png',
+        time: '20 Mins Ago',
+        subtitle: 'Front End Developer',
+        title: '<strong>Sab Zirot </strong>',
+      },
+         {
+        avatar: 'image-4.png',
+        time: '40 Mins Ago',
+        subtitle: 'HR ',
+        title: '<strong>Soui Liuorf </strong>',
+      },
+     
+  ]
+})
+}
+</script>
+
+<style>
+
+</style>
--- src/views/TheHome.vue
+++ src/views/TheHome.vue
@@ -1,19 +1,21 @@
 <template>
   <v-app :style="{background: $vuetify.theme.themes.dark.background}">
 <the-side-bar/>
+<the-right-side-bar/>
   </v-app>
 </template>
 
 <script>
 
 import TheSideBar from '@/components/TheSideBar.vue'
+import TheRightSideBar from '@/components/TheRightSideBar.vue'
 
   export default {
     name: 'TheHome',
 
     components: {
-    
-    TheSideBar
+    TheSideBar,
+    TheRightSideBar
 },
   }
 </script>
