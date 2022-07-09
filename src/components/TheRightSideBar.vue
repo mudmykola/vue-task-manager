f1855f8051c84915536ad84a1a7c03370b19248a
Mykola <46628437+mudmykola@users.noreply.github.com>
2022-07-09 11:46:01 +0300

    done=>rightsidebar

--- src/components/TheRightSideBar.vue
+++ src/components/TheRightSideBar.vue
@@ -1,41 +1,136 @@
 <template>
-  <div></div>
+  <v-navigation-drawer app color="#14141c" right width="250">
+    <v-toolbar color="rgba(0,0,0,0)" flat>
+      <v-icon class=" mr-4" color="#7c7c7f">fas fa-search</v-icon>
+      <v-badge class="mr-8" overlap bordered color="red" dot>
+        <v-icon color="#7c7c7f">far fa-bell</v-icon>
+      </v-badge>
+      <v-divider vertical inset color="#7c7c7f"></v-divider>
+      <v-spacer></v-spacer>
+      <v-badge bordered bottom color="#49d9a0" dot offset-x="10" offset-y="10">
+        <v-avatar class=" ml-n3" size="30">
+          <v-img src="../assets/image-2.png"></v-img>
+        </v-avatar>
+      </v-badge>
+      <v-btn text color="#7c7c7f">Linda</v-btn>
+      <v-btn class=" ml-n3" icon dark>
+        <v-icon x-small color="#7c7c7f">fas fa-chevron-down</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <strong class="ml-3 white--text">Overall Progress</strong><br>
+    <strong class=" ml-3 grey--text caption">Pet Project</strong>
+    <div class="mt-5 mb-5 text-center">
+      <v-progress-circular class="mt-5" :size="100" :width="12" :value="50" color="red">50%</v-progress-circular>
+    </div>
+    <strong class="ml-3 white--text">Recent Activites</strong><br>
+    <strong class=" ml-3 grey--text caption">07 June 2022</strong>
+    <v-list three-line>
+      <v-list-item-group>
+        <template v-for="(item) in files">
+          <v-list-item :key="item.title">
+            <v-list-item-avatar>
+              <img :src="item.avatar" />
+            </v-list-item-avatar>
+            <template>
+              <v-list-item-content>
+                <v-list-item-title class="white--text" v-html="item.title"></v-list-item-title>
+                <v-list-item-subtitle class=" grey--text" v-text="item.subtitle"></v-list-item-subtitle>
+                <v-list-item-subtitle class=" grey--text" v-text="item.time"></v-list-item-subtitle>
+              </v-list-item-content>
+            </template>
+          </v-list-item>
+        </template>
+      </v-list-item-group>
+    </v-list>
+    <v-row>
+      <v-col cols="12" sm="6">
+        <v-card class=" d-flex align-center ml-4" height="60px" width="120px" color="#21212b">
+          <v-row>
+            <v-col cols="12" sm="12">
+              <v-list-item class="text-center" two-line>
+                <v-list-item-content>
+                  <v-list-item-title class=" white--text">152</v-list-item-title>
+                  <v-list-item-subtitle  class="grey--text caption mt-2" >Total Progress</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-card class=" d-flex align-center mx-2" height="60px" width="120px" color="#a9ddd8">
+          <v-row>
+            <v-col cols="12" sm="12">
+              <v-list-item class="text-center" two-line>
+                <v-list-item-content>
+                  <v-list-item-title class=" black--text ">300</v-list-item-title>
+                  <v-list-item-subtitle  class="black--text caption mt-2" >Upcomping</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+       <v-col cols="12" sm="6">
+        <v-card class=" d-flex align-center ml-4" height="60px" width="120px" color="#21212b">
+          <v-row>
+            <v-col cols="12" sm="12">
+              <v-list-item class="text-center" two-line>
+                <v-list-item-content>
+                  <v-list-item-title class=" white--text">200</v-list-item-title>
+                  <v-list-item-subtitle  class="grey--text caption mt-2" >In Progress</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-card class=" d-flex align-center mx-2" height="60px" width="120px" color="#21212b">
+          <v-row>
+            <v-col cols="12" sm="12">
+              <v-list-item class="text-center" two-line>
+                <v-list-item-content>
+                  <v-list-item-title class=" white--text ">452</v-list-item-title>
+                  <v-list-item-subtitle  class="grey--text caption mt-2" >Complete</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-navigation-drawer>
 </template>
 
 <script>
 export default {
-data: () =>({
-  files: [
-         {
-        avatar: 'image-1.png',
+  data: () => ({
+    files: [
+      {
+        avatar: 'http://cdn.vuetifyjs.com/images/lists/1.jpg',
         time: '2 Mins Ago',
         subtitle: 'QA Automatic',
         title: '<strong>Miki Roi  </strong>',
       },
-           {
-        avatar: 'image-2.png',
+      {
+        avatar: "http://cdn.vuetifyjs.com/images/lists/5.jpg",
         time: '10 Mins Ago',
         subtitle: 'Backand Developer',
         title: '<strong>Simo Muore </strong>',
       },
-         {
-        avatar: 'image-3.png',
+      {
+        avatar: 'http://cdn.vuetifyjs.com/images/lists/3.jpg',
         time: '20 Mins Ago',
         subtitle: 'Front End Developer',
         title: '<strong>Sab Zirot </strong>',
       },
-         {
-        avatar: 'image-4.png',
-        time: '40 Mins Ago',
-        subtitle: 'HR ',
-        title: '<strong>Soui Liuorf </strong>',
-      },
-     
-  ]
-})
+
+
+    ]
+  })
 }
 </script>
 
 <style>
-
 </style>
--- src/views/TheHome.vue
+++ src/views/TheHome.vue
@@ -2,6 +2,28 @@
   <v-app :style="{background: $vuetify.theme.themes.dark.background}">
 <the-side-bar/>
 <the-right-side-bar/>
+<v-container>
+  <v-toolbar color="rgba(0,0,0,0)" flat>
+    <v-toolbar-title class="mt-n4 white--text ">Task Manager</v-toolbar-title>
+  </v-toolbar>
+  <v-toolbar class="mt-n6" color="rgba(0,0,0,0)" flat dark >
+    <div>
+      <v-icon class="mr-2" color="#7c7c7f">far fa-calendar-alt</v-icon>
+      <v-badge content="2" color="red">
+        <span class="caption grey--text mt-2">DES 15, 2022</span>
+      </v-badge>
+    </div>
+    <v-spacer></v-spacer>
+    <div color="rgba(0,0,0,0)" >
+      <v-tabs color="#a9ddd8" right background-color="#171721">
+        <v-tab >Day</v-tab>
+        <v-tab >Week</v-tab>
+        <v-tab >Month</v-tab>
+        <v-tab >Year</v-tab>
+      </v-tabs>
+    </div>
+  </v-toolbar>
+</v-container>
   </v-app>
 </template>
 
@@ -19,3 +41,15 @@ import TheRightSideBar from '@/components/TheRightSideBar.vue'
 },
   }
 </script>
+
+
+<style scoped>
+.theme--light.v-timeline::before{
+  background: #7c7c7f;
+}
+
+.border{
+  background: #22222c;
+  border-radius: 50%;
+}
+</style>
